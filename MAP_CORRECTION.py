810a8f052dc839358e8776cf09f0dcd2d6f2ee3c
codeliveyou <codetankstn@gmail.com>
2025-04-06 20:00:48 +0900

    complete

--- MAP_CORRECTION.py
+++ MAP_CORRECTION.py
@@ -1,13 +1,40 @@
 from point import point
+import math
 
-def coordinate_transformer(ga, gb, gc, ma, mb, mc, gx, gy):
-    gd = point(gx, gy)
-    x = (gb - ga).unit() * (gd - ga) / (gb - ga).length()
-    y = (gc - ga).unit() * (gd - ga) / (gc - ga).length()
+earthr = 6378000
+R2D = 180 / math.pi
+D2R = math.pi / 180
+
+def lla_to_ned(lat0, lon0, lat, lon):
+    return point((lat - lat0) * D2R * earthr, (lon - lon0) * D2R * (earthr * math.cos(lat0 * D2R)))
+
+def ned_to_lla(lat0, lon0, n, e):
+    return point(n / earthr * R2D + lat0, e / (earthr *  math.cos(lat0 * D2R)) * R2D + lon0)
+
+def global_to_map(ga, gb, gc, ma, mb, mc, gx, gy):
+    na = lla_to_ned(ga.x, ga.y, ga.x, ga.y)
+    nb = lla_to_ned(ga.x, ga.y, gb.x, gb.y)
+    nc = lla_to_ned(ga.x, ga.y, gc.x, gc.y)
+    nd = lla_to_ned(ga.x, ga.y, gx, gy)
+
+    x = (nb - na).unit() * (nd - na) / (nb - na).length()
+    y = (nc - na).unit() * (nd - na) / (nc - na).length()
     md = (mb - ma) * x + (mc - ma) * y
     return [md.x, md.y]
 
+def map_to_global(ma, mb, mc, ga, gb, gc, mx, my):
+    md = point(mx, my)
+    x = (mb - ma).unit() * (md - ma) / (mb - ma).length()
+    y = (mc - ma).unit() * (md - ma) / (mc - ma).length()
 
+    na = lla_to_ned(ga.x, ga.y, ga.x, ga.y)
+    nb = lla_to_ned(ga.x, ga.y, gb.x, gb.y)
+    nc = lla_to_ned(ga.x, ga.y, gc.x, gc.y)
+    nd = (nb - na) * x + (nc - na) * y
+
+    gd = ned_to_lla(ga.x, ga.y, nd.x, nd.y)
+
+    return [gd.x, gd.y]
 
 ga = point(39.053078, 125.769001)
 gb = point(39.053301, 125.768969)
@@ -17,4 +44,6 @@ ma = point(0, 0)
 mb = point(0, 599)
 mc = point(899, 0)
 
-print(coordinate_transformer(ga, gb, gc, ma, mb, mc, 39.053228771840985, 125.76913886850882))
+print(global_to_map(ga, gb, gc, ma, mb, mc, 39.053228, 125.769138))
+
+print(map_to_global(ma, mb, mc, ga, gb, gc, 316.35, 368.36))
